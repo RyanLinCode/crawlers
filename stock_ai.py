74b33e1f3bbfc4ac1e752fae77e0e9133eb42203
RyanLin <Willie003652@gmail.com>
2021-07-13 22:12:03 +0800

    stock_ai

---
+++ stock_ai.py
@@ -0,0 +1,11 @@
+import requests
+from bs4 import BeautifulSoup
+
+import pprint
+
+r = requests.get('https://chart.stock-ai.com/history?symbol=%5ETWII&resolution=D&from=1564604150&to=1598818610', verify=False)
+if r.status_code == requests.codes.ok:
+    data = r.json()
+
+    zipped = zip(data['t'], data['o'], data['h'], data['l'], data['o'], data['v'])
+    pprint.pprint(list(zipped))
